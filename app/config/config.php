ac7c60e2a037d45b4278bc91424ce46022ff87ff
Joshua-owolabi <primtech.co@gmail.com>
2018-12-23 12:13:48 +0100

    changes to pages.php

--- app/config/config.php
+++ app/config/config.php
@@ -3,7 +3,7 @@
 //DB PARAMS 
  define('DB_HOST', 'sql203.epizy.com');
  define('DB_USER', 'epiz_23173129');
- define('DB_PASS', 'DropCOde99');
+ define('DB_PASS', 'DropCode99');
  define('DB_NAME', 'epiz_23173129_dropcode'); 
 // APP ROOT
 define('APPROOT', dirname(dirname(__FILE__)));
--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -5,10 +5,6 @@ class Pages extends Controller {
     public function __construct() {
     }
     public function index() {
-
-        $data = [
-            'title' => "Home Page"
-        ];
         // loading the index page view
         $this->view('pages/index', $data);
     }
